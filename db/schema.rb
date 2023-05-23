40f5bee2410662664905c693796f3216f82b119a
Andrew Bowman <andrewtbowman@gmail.com>
2023-05-22 20:07:22 -0400

    db: add password_digest

--- db/schema.rb
+++ db/schema.rb
@@ -38,6 +38,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_11_233529) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
