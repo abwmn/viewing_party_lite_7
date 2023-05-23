2a33bf18d095c50b73d12622e55b8569f9fbadf5
Andrew Bowman <andrewtbowman@gmail.com>
2023-05-22 20:06:40 -0400

    feat: user registration with password, happy and sad paths

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,10 @@ class User < ApplicationRecord
   has_many :partygoings, dependent: :destroy
   has_many :invited_parties, through: :partygoings, source: :party  
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
   validates :email, presence: true, 
             uniqueness: { case_sensitive: false }, 
             format: {with: URI::MailTo::EMAIL_REGEXP }
+
+  has_secure_password
 end
